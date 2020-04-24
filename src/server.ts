bdef801c62ab371d67387d7f248fb361a4da82db
santosjn <jeansjcsp@gmail.com>
2020-04-24 09:28:33 -0300

    Node.js project using typescript, editorconfig, eslint and prettier.

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+
+const port = 3333;
+app.listen(port, () => {
+  console.log(`Server started an port ${port}`);
+});
